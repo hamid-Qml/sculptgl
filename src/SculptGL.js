dfa621f82412bb3ca4b13581c8694619e602d75a
hamid-Qml <80735548+hamid-Qml@users.noreply.github.com>
2024-12-07 12:10:55 +0500

    Update SculptGL.js
    endpoint changed

--- src/SculptGL.js
+++ src/SculptGL.js
@@ -113,7 +113,7 @@ class SculptGL extends Scene {
       dialogButtons.style.display = 'none';
 
       try {
-        const response = await fetch('https://63d4-72-255-38-118.ngrok-free.app/generate?media_type=image', {
+        const response = await fetch('https://d143-72-255-38-118.ngrok-free.app/generate?media_type=image', {
           method: 'POST',
           headers: { accept: 'application/json' },
           body: formData,
